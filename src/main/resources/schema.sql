479f5bd28c52e3876ca2b5cb776656bfc0f66cef
linux- local <sunilpoudel11@gmail.com>
2018-11-25 06:55:50 +0545

    make it better

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE office (
+  office_id BIGINT PRIMARY KEY,
+  addressline1 VARCHAR(255),
+  addressline2 VARCHAR(255),
+  city_name VARCHAR(255),
+  country VARCHAR(255),
+  phone_number VARCHAR(255),
+  postal_code VARCHAR(255),
+  state VARCHAR(255),
+  territory VARCHAR(255)
+);
